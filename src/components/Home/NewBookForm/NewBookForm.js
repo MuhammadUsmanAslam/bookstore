64c8d1b410e269e2ea8db11b2fe89711755e1a56
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-04-07 19:49:31 +0500

    Fix newBookForm design issues

--- src/components/Home/NewBookForm/NewBookForm.js
+++ src/components/Home/NewBookForm/NewBookForm.js
@@ -15,54 +15,56 @@ function NewBookForm() {
     <form className="new-book-form">
       <hr />
       <h2>Add New Book</h2>
-      <input
-        type="text"
-        placeholder="Book Title"
-        value={newBook.title}
-        onChange={(e) => {
-          setNewBook({ ...newBook, title: e.target.value });
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Book Author"
-        value={newBook.author}
-        onChange={(e) => {
-          setNewBook({ ...newBook, author: e.target.value });
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Book Category"
-        value={newBook.category}
-        onChange={(e) => {
-          setNewBook({ ...newBook, category: e.target.value });
-        }}
-      />
-      <button
-        className="btn new-book-form--add-book-btn"
-        type="submit"
-        onClick={(e) => {
-          e.preventDefault();
-          const newBookToAdd = {
-            ...newBook,
-            item_id: uuidv4(),
-          };
-          if (newBook.title && newBook.author && newBook.category) {
-            dispatch(addNewBook(newBookToAdd))
-              .then(() => {
-                dispatch(addBook(newBookToAdd));
-                setNewBook({
-                  title: '', author: '', item_id: -1, category: '',
+      <div className="new-book-form-body">
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={newBook.title}
+          onChange={(e) => {
+            setNewBook({ ...newBook, title: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Book Author"
+          value={newBook.author}
+          onChange={(e) => {
+            setNewBook({ ...newBook, author: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Book Category"
+          value={newBook.category}
+          onChange={(e) => {
+            setNewBook({ ...newBook, category: e.target.value });
+          }}
+        />
+        <button
+          className="btn new-book-form--add-book-btn"
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            const newBookToAdd = {
+              ...newBook,
+              item_id: uuidv4(),
+            };
+            if (newBook.title && newBook.author && newBook.category) {
+              dispatch(addNewBook(newBookToAdd))
+                .then(() => {
+                  dispatch(addBook(newBookToAdd));
+                  setNewBook({
+                    title: '', author: '', item_id: -1, category: '',
+                  });
                 });
-              });
-          } else {
-            alert('Please Add a book first');
-          }
-        }}
-      >
-        ADD BOOK
-      </button>
+            } else {
+              alert('Please Add a book first');
+            }
+          }}
+        >
+          ADD BOOK
+        </button>
+      </div>
     </form>
   );
 }
